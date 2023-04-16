a544445d270f6ba2d7ed65fa6a6d8f41b4a11a4c
Nicholas Ade <90573287+Nadir-Lafayette@users.noreply.github.com>
2023-04-16 16:15:58 -0400

    this is multiply and divide for the sim.

--- sim/common/bfloat.cpp
+++ sim/common/bfloat.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <bitset>
 
-#include <iostream>
-#include <bitset>
-
 // get float "in-memory" to exploit iee754 binary representation of floating point values
 // use a u to trick compiler into letting you access float's bits directly
 // bitwise operations cannot be done directly on iee754 representations per compiler settings
@@ -107,18 +104,38 @@ public:
     {
         uint16_t a_exp = a.in_mem.parts.exponent;
         uint16_t b_exp = b.in_mem.parts.exponent;
-        uint16_t a_mantissa = a.in_mem.parts.mantissa | 0x0080; // Add implicit bit
-        uint16_t b_mantissa = b.in_mem.parts.mantissa | 0x0080; // Add implicit bi
+        uint16_t a_mantissa = a.in_mem.parts.mantissa;
+        uint16_t b_mantissa = b.in_mem.parts.mantissa;
+
+        bool product_sign = a.in_mem.parts.sign ^ b.in_mem.parts.sign;
+        // Nan check
+        if ((a_exp == 0xFF && a_mantissa > 1) || (b_exp == 0xFF && b_mantissa > 1))
+        {
+            std::cout << "HERE IN NAN" << std::endl;
+            return MyFloat(1, 0xff, product_sign);
+        }
+
+        // Zero check
+        if ((a_exp == 0 && a_mantissa == 0) || (b_exp == 0 && b_mantissa == 0))
+        {
+            std::cout << "HERE IN ZERO" << std::endl;
+            return MyFloat(0, 0, product_sign);
+        }
+        // infinity check
+        if (a_exp == 0xFF || b_exp == 0xff)
+        {
+            std::cout << "HERE IN infinity" << std::endl;
+            return MyFloat(0, 0xFF, product_sign);
+        }
+        a_mantissa = a.in_mem.parts.mantissa | 0x0080; // Add implicit bit
+        b_mantissa = b.in_mem.parts.mantissa | 0x0080; // Add implicit bi
 
         std::bitset<8> bits(a_exp);
         std::cout << "Binary a exp: " << bits << std::endl;
 
-        bool product_sign = a.in_mem.parts.sign ^ b.in_mem.parts.sign;
+        std::bitset<8> bis(a_mantissa);
+        std::cout << "Binary a mantissa: " << bis << std::endl;
 
-        if (a_exp == 0xFF || b_exp == 0xff)
-        {
-            return MyFloat(0, 0xFF, product_sign);
-        }
         // Multiply mantissas
         uint32_t product_mantissa = static_cast<uint32_t>(a_mantissa) * static_cast<uint32_t>(b_mantissa);
 
@@ -148,74 +165,171 @@ public:
     {
         uint16_t a_exp = a.in_mem.parts.exponent;
         uint16_t b_exp = b.in_mem.parts.exponent;
-        std::bitset<8> bits(b_exp);
-        std::cout << "Binary b exp: " << bits << std::endl;
-        uint16_t a_mantissa = a.in_mem.parts.mantissa | 0x0080; // Add implicit bit
-        uint16_t b_mantissa = b.in_mem.parts.mantissa | 0x0080; // Add implicit bit
-
         bool quotient_sign = a.in_mem.parts.sign ^ b.in_mem.parts.sign;
-
         // Check if divisor is zero
-        if (b_exp == 0 && b_mantissa == 0)
+        if (b_exp == 0 && b.in_mem.parts.mantissa == 0)
         {
-            std::cout << "HERE" << std::endl;
-            return MyFloat(0, 0xFF, quotient_sign); // Return infinity with the appropriate sign
+            return MyFloat(1, 0xFF, 0); // Return nam
         }
-
-        // Check for infinity or zero in dividend
-        if (a_exp == 0xFF || a_exp == 0)
+        // check if a is zero
+        if (a_exp == 0 && a.in_mem.parts.mantissa == 0)
         {
             return MyFloat(0, a_exp, quotient_sign);
         }
+        // check if a is infinity and b is infinity
+        if ((a.in_mem.f == 0x7f80 || a.in_mem.f == 0xff80) && (b.in_mem.f == 0x7f80 || b.in_mem.f == 0xff80))
+        {
+            return MyFloat(1, a_exp, quotient_sign);
+        }
 
+        if (a_exp == 0xff)
+        {
+            return MyFloat(0, a_exp, quotient_sign);
+        }
+        // If divisor == inf return 0
+        if (b_exp == 0xff)
+        {
+            return MyFloat(0, 0, quotient_sign);
+        }
+        uint16_t a_mantissa = a.in_mem.parts.mantissa | 0x0080; // Add implicit bit
+        uint16_t b_mantissa = b.in_mem.parts.mantissa | 0x0080; // Add implicit bit
+
+        std::bitset<8> bits(a_exp);
+        std::cout << "Binary a mantissa: " << bits << std::endl;
+        std::bitset<8> bs(b_exp);
+        std::cout << "Binary b  mantissa: " << bs << std::endl;
         // Subtract exponents
-        int quotient_exp = a_exp - b_exp + 127;
-
+        int quotient_exp = (a_exp) - (b_exp) + 127;
+        std::bitset<8> bis(quotient_exp);
+        std::cout << "Binary quotient_exp: " << bis << std::endl;
+        uint32_t quotient_mantissa;
         // Divide mantissas
-        uint32_t quotient_mantissa = (static_cast<uint32_t>(a_mantissa) << 8) / static_cast<uint32_t>(b_mantissa);
 
-        quotient_mantissa = (quotient_mantissa + 0x40) >> 8;
-
-        // Round to nearest even (round half to even)
-        if ((quotient_mantissa & 0x1) != 0 && (quotient_mantissa & 0x7F) == 0x40)
+        if (b.in_mem.parts.mantissa == 0)
         {
-            quotient_mantissa--;
-        }
-        else if ((quotient_mantissa & 0x7F) == 0x40)
-        {
-            quotient_mantissa++;
-        }
 
-        if (quotient_mantissa & 0x0100)
-        { // Check if the implicit bit shifted to the left
-            quotient_mantissa >>= 1;
-            quotient_exp++;
+            quotient_mantissa = a_mantissa;
         }
         else
         {
-            quotient_mantissa &= 0x7F; // Remove the implicit bit
+            printf("HHHHHHIIII\n");
+            quotient_mantissa = (static_cast<uint32_t>(a_mantissa)) / static_cast<uint32_t>(b_mantissa);
         }
+
+        std::bitset<8> bem(quotient_mantissa);
+        std::cout << "Binary quotient_mantissa: " << bem << std::endl;
+
+        // IDK i need this it is super bad but it make it work with 21/7
+        if (quotient_mantissa == 0)
+        {
+            quotient_exp--;
+            quotient_mantissa = 0x40;
+        }
+
         return MyFloat(quotient_mantissa, quotient_exp, quotient_sign);
     }
 };
 
+union FloatToUInt32
+{
+    float f;
+    uint32_t u;
+};
 int main()
 {
-    float a = 8;
-    float b = 0;
-    std::cout << a << std::endl;
-
+    union FloatToUInt32 a;
+    a.u = 0x7f800000;
+    float b = 1;
+    printf("a/b = %f\n", a.f);
     std::bitset<sizeof(float) * 8> bits(*reinterpret_cast<unsigned long *>(&a));
-    std::cout << "Binary representation of " << a << " is \n"
+    std::cout << "Binary representation of " << a.f << " is \n"
               << bits << std::endl;
     std::cout << "Binary representation of " << b << " is \n"
               << bits << std::endl;
 
-    MyFloat bfloat_version_of_a(a);
+    MyFloat bfloat_version_of_a(a.f);
     MyFloat bfloat_version_of_b(b);
-    MyFloat c = bfloat_version_of_a / bfloat_version_of_b;
+    MyFloat c = bfloat_version_of_b / bfloat_version_of_a;
 
     // You can now print the result stored in c or perform other operations with it.
 
     return 0;
 }
+
+/*
+
+#include <iostream>
+
+// function to print binary representation of a float number
+void printBinary(float num) {
+    unsigned char *p = reinterpret_cast<unsigned char*>(&num);
+    for (int i = sizeof(float) - 1; i >= 0; i--) {
+        for (int j = 7; j >= 0; j--) {
+            std::cout << ((p[i] >> j) & 1);
+        }
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+}
+
+// function to add two binary representations of float numbers
+float addBinary(float a, float b) {
+    unsigned int a_bits = *(unsigned int*)&a;
+    unsigned int b_bits = *(unsigned int*)&b;
+
+    // separate sign, exponent, and mantissa of a and b
+    int a_sign = (a_bits >> 31) & 1;
+    int a_exp = ((a_bits >> 23) & 0xff) - 127;
+    int a_mantissa = (a_bits & 0x7fffff) | 0x800000;
+    int b_sign = (b_bits >> 31) & 1;
+    int b_exp = ((b_bits >> 23) & 0xff) - 127;
+    int b_mantissa = (b_bits & 0x7fffff) | 0x800000;
+
+    // align mantissas by shifting the smaller exponent to the larger exponent
+    if (a_exp < b_exp) {
+        a_mantissa >>= (b_exp - a_exp);
+        a_exp = b_exp;
+    } else {
+        b_mantissa >>= (a_exp - b_exp);
+        b_exp = a_exp;
+    }
+
+    // add mantissas and adjust exponent if necessary
+    int sum_mantissa = a_mantissa + b_mantissa;
+    if (sum_mantissa & 0x1000000) {
+        sum_mantissa >>= 1;
+        a_exp++;
+    }
+
+    // build binary representation of result
+    int result_sign = a_sign;
+    int result_exp = a_exp;
+    int result_mantissa = sum_mantissa & 0x7fffff;
+
+    // combine sign, exponent, and mantissa into a float number
+    unsigned int result_bits = (result_sign << 31) | ((result_exp + 127) << 23) | result_mantissa;
+    float result = *(float*)&result_bits;
+
+    return result;
+}
+
+int main() {
+    float a = 0.25f;
+    float b = 100.0f;
+
+    std::cout << "a = " << a << " in binary: ";
+    printBinary(a);
+
+    std::cout << "b = " << b << " in binary: ";
+    printBinary(b);
+
+    float sum = addBinary(a, b);
+
+    std::cout << "a + b = " << sum << " in binary: ";
+    printBinary(sum);
+
+    return 0;
+}
+
+
+*/
