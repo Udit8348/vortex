fb717af4dc0d7fb40b4210e0512d1342ac4a72cb
Udit Subramanya <usubramanya3@gatech.edu>
2023-04-25 15:09:11 -0400

    bf force float add in iadd for debugging purposes

---
+++ tests/regression/brainfloat/kernel.c
@@ -0,0 +1,66 @@
+#include <stdint.h>
+#include <math.h>
+#include <vx_intrinsics.h>
+#include <vx_print.h>
+#include <vx_spawn.h>
+#include "common.h"
+
+typedef void (*PFN_Kernel)(int task_id, kernel_arg_t* arg);
+
+void kernel_iadd(int task_id, kernel_arg_t* arg) {
+	// vx_printf("IADD TEST CASE\n");
+	uint32_t count    = arg->task_size;
+	int32_t* src0_ptr = (int32_t*)arg->src0_addr;
+	int32_t* src1_ptr = (int32_t*)arg->src1_addr;
+	int32_t* dst_ptr  = (int32_t*)arg->dst_addr;	
+	uint32_t offset = task_id * count;
+
+	// float test = vx_bfadd_16(-1.0, 1.0);
+	float e = 1.0;
+	float test = e + src0_ptr[0]; // I want this instruction to get compiled as a floating point instrucion
+	// dont want test to get optimized out
+	vx_printf("ONE PLUS ONE %f\n", test);
+	
+	if (test == 0.1) {
+		test = 0;
+	}
+
+
+	for (uint32_t i = 0; i < count; ++i) {
+		int32_t a = src0_ptr[offset+i];
+		int32_t b = src1_ptr[offset+i];
+		int32_t c = a + b;
+		dst_ptr[offset+i] = c;
+	}
+}
+
+void kernel_fadd(int task_id, kernel_arg_t* arg) {
+	uint32_t count  = arg->task_size;
+	float* src0_ptr = (float*)arg->src0_addr;
+	float* src1_ptr = (float*)arg->src1_addr;
+	float* dst_ptr  = (float*)arg->dst_addr;
+	// float* dst_ptr  = (float*)arg->dst_addr;	
+	uint32_t offset = task_id * count;
+
+	for (uint32_t i = 0; i < count; ++i) {
+		// compiles into float loads (flw)
+		float a = src0_ptr[offset+i];
+		float b = src1_ptr[offset+i];
+		
+		
+		float c = a + b;
+
+		// compiles into int add for offset then, float store (fsw)
+		dst_ptr[offset+i] = c;
+	}
+}
+
+static const PFN_Kernel sc_tests[] = {
+	kernel_iadd,
+	kernel_fadd
+};
+
+void main() {
+	kernel_arg_t* arg = (kernel_arg_t*)KERNEL_ARG_DEV_MEM_ADDR;
+	vx_spawn_tasks(arg->num_tasks, (vx_spawn_tasks_cb)sc_tests[arg->testid], arg);
+}
---
+++ tests/regression/brainfloat/testcases.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <iostream>
+#include <math.h>
+#include <limits>
+
+union Float_t {    
+    float f;
+    int   i;
+    struct {
+        uint32_t man  : 23;
+        uint32_t exp  : 8;
+        uint32_t sign : 1;
+    } parts;
+};
+
+inline float fround(float x, int32_t precision = 8) {
+  auto power_of_10 = std::pow(10, precision);
+  return std::round(x * power_of_10) / power_of_10;
+}
+
+inline bool almost_equal_eps(float a, float b, int ulp = 128) {
+  auto eps = std::numeric_limits<float>::epsilon() * (std::max(fabs(a), fabs(b)) * ulp);
+  auto d = fabs(a - b);
+  if (d > eps) {
+    std::cout << "*** almost_equal_eps: d=" << d << ", eps=" << eps << std::endl;
+    return false;
+  }
+  return true;
+}
+
+inline bool almost_equal_ulp(float a, float b, int32_t ulp = 6) {
+  Float_t fa{a}, fb{b};
+  auto d = std::abs(fa.i - fb.i);
+  if (d > ulp) {
+    std::cout << "*** almost_equal_ulp: a=" << a << ", b=" << b << ", ulp=" << d << ", ia=" << std::hex << fa.i << ", ib=" << fb.i << std::endl;
+    return false;
+  }
+  return true;
+}
+
+inline bool almost_equal(float a, float b) {
+  if (a == b)
+    return true;
+  /*if (almost_equal_eps(a, b))
+    return true;*/
+  return almost_equal_ulp(a, b);
+}
+
+class ITestCase {
+public:
+  ITestCase() {}
+  virtual ~ITestCase() {}
+
+  virtual void setup(int n, void* src1, void* src2)  = 0;  
+  virtual int verify(int n, void* dst, const void* src1, const void* src2) = 0;
+};
+
+class Test_IADD : public ITestCase {
+public:
+
+  void setup(int n, void* src1, void* src2) override {
+    auto a = (int32_t*)src1;
+    auto b = (int32_t*)src2;
+    for (int i = 0; i < n; ++i) {
+      a[i] = n/2 - i;
+      b[i] = n/2 + i;
+    }
+  }
+  
+  int verify(int n, void* dst, const void* src1, const void* src2) override {
+    int errors = 0;
+    auto a = (int32_t*)src1;
+    auto b = (int32_t*)src2;
+    auto c = (int32_t*)dst;
+    for (int i = 0; i < n; ++i) {
+      auto ref = a[i] + b[i]; 
+      if (c[i] != ref) {
+        std::cout << "error at result #" << i << ": expected=" << ref << ", actual=" << c[i] << ", a=" << a[i] << ", b=" << b[i] << std::endl;
+        ++errors;
+      }
+    }
+    return errors;
+  }
+};
+
+class Test_FADD : public ITestCase {
+public:
+
+  void setup(int n, void* src1, void* src2) override {
+    auto a = (float*)src1;
+    auto b = (float*)src2;
+    for (int i = 0; i < n; ++i) {
+      a[i] = fround((n - i) * (1.0f/n));
+      b[i] = fround((n + i) * (1.0f/n));
+    }
+  }
+  
+  int verify(int n, void* dst, const void* src1, const void* src2) override {
+    int errors = 0;
+    auto a = (float*)src1;
+    auto b = (float*)src2;
+    auto c = (float*)dst;
+    for (int i = 0; i < n; ++i) {
+      auto ref = a[i] + b[i]; 
+      std::cout << "expected=" << ref << ", actual=" << c[i] << ", a=" << a[i] << ", b=" << b[i] << std::endl;
+      if (!almost_equal(c[i], ref)) {
+        std::cout << "error at result #" << i << ": expected=" << ref << ", actual=" << c[i] << ", a=" << a[i] << ", b=" << b[i] << std::endl;
+        ++errors;
+      }
+    }
+    return errors;
+  }
+};
